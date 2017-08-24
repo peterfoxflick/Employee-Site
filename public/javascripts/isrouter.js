c112849ee3438471d4bb7a0dbe64ec44fec2d934
Bryan <starwarsgeek0975@gmail.com>
2017-08-24 12:16:13 -0600

    finished Issue Routing Page

--- public/javascripts/isrouter.js
+++ public/javascripts/isrouter.js
@@ -3,93 +3,148 @@ const list = {
   "regist": {
     ext: 1000,
     title: "Records and Registration",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "grades": {
     ext: 1025,
     title: "Records and Registration",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "photos": {
     ext: 7232,
     title: "Photo Services",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "edTech": {
     ext: 7236,
     title: "EdTech Lab",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "TestSched": {
     ext: 1760,
     title: "Test Scheduling",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "Copyright": {
     ext: 9544,
     title: "Nate Wise",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "AV": {
     ext: 3150,
     title: "A/V Event Services",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "CC": {
     ext: 3218,
     title: "Close Captioning",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "hprinter": {
     ext: "N/A",
     title: "Support Center",
-    message: "Create a Ticket for the Support Center"
+    message: "Create a Ticket for the Support Center",
+    ticketURL: "https://td.byui.edu/TDClient/Requests/TicketRequests/NewForm?ID=~K98XgepGOY_",
+    ticket: "Printer Ticket"
   },
   "BSC": {
     ext: 1411,
     title: "BSC",
-    message: "Transer to the BSC"
+    message: "Transer to the BSC",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "phone": {
     ext: "N/A",
     title: "Campus phones",
-    message: "Submit a Phone Ticket"
+    message: "Submit a Phone Ticket",
+    ticketURL: "https://td.byui.edu/TDClient/Requests/TicketRequests/NewForm?ID=DP6wOamgvuU_",
+    ticket: "Phone Ticket"
   },
   "hcp": {
     ext: "N/A",
     title: "N/A",
-    message: "The University has no resources for helping faculty with personal computers"
+    message: "The University has no resources for helping faculty with personal computers",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "hcc": {
     ext: 7070,
-    title: "Classroom Technology",
-    message: "Create a ticket in the support center. If an emergency, transfer to Classroom Tech"
+    title: "Tech Support",
+    message: "If it is an emergency regarding in-class tech, then call Classroom Tech at 7070. Otherwise create a ticket.",
+    ticketURL: "https://td.byui.edu/TDClient/Requests/TicketRequests/NewForm?ID=5-JQTG-M6mk_",
+    ticket: "Classroom Tech Ticket"
   },
   "sequella": {
     ext: 9516,
     title: "John Fackrell",
-    message: "You can also try Matt Miles at x9541"
+    message: "You can also try Matt Miles at x9541",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "iplan": {
     ext: 9822,
     title: "James Gordon",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "mac": {
     ext: 7231,
     title: "Mac Lab",
-    message: "The Commons in the Library"
+    message: "The Commons in the Library",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "FTC": {
     ext: 7230,
     title: "Faculty Technology Center",
-    message: "Looks like you're out of luck ;)"
+    message: "Looks like you're out of luck ;)",
+    ticketURL: "#",
+    ticket: "N/A"
   },
   "VMWare": {
     ext: 7219,
     title: "Gordon Smith",
-    message: "N/A"
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
+  },
+  kaltura: {
+    ext: 3253,
+    title: "Media Transfer",
+    message: "If Media Transfer is unavailable, you can call Chris Smelcer (x3208)",
+    ticketURL: "#",
+    ticket: "N/A"
+  },
+  courseSupport: {
+    ext: 1866,
+    title: "Course Support",
+    message: "N/A",
+    ticketURL: "#",
+    ticket: "N/A"
+  },
+  ilearnIT: {
+    ext: 7217,
+    title: "I-Learn IT (Level 3)",
+    message: "First create a ticket. If needed, call Dane Bohman at the above extension",
+    ticketURL: "https://td.byui.edu/TDClient/Requests/TicketRequests/NewForm?ID=Dl5eALqHGV8_",
+    ticket: "I-Learn IT"
   }
-
-
 };
--- public/javascripts/issuerouting.js
+++ public/javascripts/issuerouting.js
@@ -2,47 +2,55 @@ let query = '';
 
 $(document).ready(function(){
     $('#clear').click(function(){
-      query = '';
-      $('#d').html('');
-       $('#buttons').children().addClass("fadeOut").hide();
-       $('#tier1').removeClass('fadeOut').show().addClass("fadeIn");
+       query = '';
+       $('#d').html('');
+       $('#buttons').children().addClass("fadeOutDown").hide();
+       $('#tier1').removeClass('fadeOutDown').show().addClass("fadeInLeft");
   });
 });
 
 
 function showTier2(n,q) {
-  $('#tier1').addClass("animated fadeOut");
+  $('#tier1').addClass("animated fadeOutDown");
   setTimeout(function(){
+    $('#' + n).removeClass('fadeOutDown').show().addClass("animated fadeInLeft");
     $('#tier1').hide();
-    $('#' + n).removeClass('fadeOut').show().addClass("animated fadeIn");
-  },700);
+  },400);
   query += q;
 }
 
 function showRoleCheck(t, q) {
-  $(`#${t}`).removeClass('fadeIn').addClass('fadeOut');
+  $(`#${t}`).removeClass('fadeInLeft').addClass('fadeOutDown');
   setTimeout(function(){
+    $('#RoleCheck').removeClass('fadeOutDown').show().addClass("animated fadeInLeft");
     $(`#${t}`).hide();
-    $('#RoleCheck').removeClass('fadeOut').show().addClass("animated fadeIn");
-  },700);
+  },400);
   query += q;
 }
 
 function showMacRoleCheck(t) {
-  $(`#${t}`).removeClass('fadeIn').addClass('fadeOut');
+  $(`#${t}`).removeClass('fadeInLeft').addClass('fadeOutDown');
   setTimeout(function(){
+    $('#RoleCheckMac').removeClass('fadeOutDown').show().addClass("animated fadeInLeft");
     $(`#${t}`).hide();
-    $('#RoleCheckMac').removeClass('fadeOut').show().addClass("animated fadeIn");
-  },700);
+  },400);
 }
 
 function showPersonalCheck(t, q){
-  $(`#${t}`).removeClass('fadeIn').addClass('fadeOut');
+  $(`#${t}`).removeClass('fadeInLeft').addClass('fadeOutDown');
   setTimeout(function(){
+    $('#personalCheck').removeClass('fadeOutDown').show().addClass("animated fadeInLeft");
     $(`#${t}`).hide();
-    $('#personalCheck').removeClass('fadeOut').show().addClass("animated fadeIn");
-  },700);
+  },400);
   query += q;
+}
+
+function showiLearnTier() {
+  $('#buttons').children().addClass("fadeOutDown");
+  setTimeout(function(){
+    $('#buttons').children().hide();
+    $('#ilearnTier').removeClass('fadeOutDown').show().addClass("animated fadeInLeft");
+  },400);
 }
 
 /**
@@ -53,13 +61,14 @@ function showPanel(q) {
   if(q === undefined) {
     q = query;
   }
-  $('#buttons').children().addClass("fadeOut").hide();
-  $('#d').html(`<div class="panel panel-accent animated fadeIn">
+  $('#buttons').children().addClass("fadeOutDown").hide();
+  $('#d').html(`<div class="panel panel-accent animated fadeInLeft">
                     <div class="panel-heading text-center">
                         <h3>${list[q].title}</h3>
                      </div>
                      <div class="panel-body">
                         <h3><strong>Phone Ext:</strong> ${list[q].ext}</h3>
+                        <h3><strong>Ticket:</strong> <a href="${list[q].ticketURL}" target="_blank">${list[q].ticket}</a></h3>
                         <h3><strong>Additional Info:</strong> ${list[q].message}</h3>
                      </div>
                  </div>`);
